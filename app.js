40dbebfa6b44ac394187951d4f3a0ebd4cc6be00
Matthew Kwon <matthew.o.kwon@gmail.com>
2022-08-25 15:48:17 -0700

    Added a function that take's user input and stores into array

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 let myLibrary = [];
 
-function Book() {
-
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead ? "read" : "not read yet"
 }
 
-function addBookToLibrary() {
-
+Book.prototype.info = function() {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}`;
 }
+
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook);
+}
+
+let book = new Book("LOTR", "J.R.R. Tolkien", 500, false);
+console.log(book.info());
