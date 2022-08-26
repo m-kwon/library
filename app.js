4905f0f5972337c430e7fd2e8bab30a385ccd43f
Matthew Kwon <matthew.o.kwon@gmail.com>
2022-08-25 22:57:49 -0700

    Created functions that will be used to toggle modal window

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
 function Book(title, author, pages, isRead) {
-  this.title = title // text
-  this.author = author // text
-  this.pages = pages // number
-  this.isRead = isRead // boolean
-}
+  this.title = title; // text
+  this.author = author; // text
+  this.pages = pages; // number
+  this.isRead = isRead; // boolean
+};
 
 function Library() {
   this.books = []; // array
-}
+};
 
 Library.prototype.addBook = function(newBook) {
   if (!this.isInLibrary(newBook)) {
-    this.books.push(newBook)
-  }
-}
+    this.books.push(newBook);
+  };
+};
 
 Library.prototype.removeBook = function(title) {
-  this.books = this.books.filter((book) => book.title !== title)
-}
+  this.books = this.books.filter((book) => book.title !== title);
+};
 
 Library.prototype.getBook = function(title) {
-  return this.books.find((book) => book.title === title)
-}
+  return this.books.find((book) => book.title === title);
+};
 
 Library.prototype.isInLibrary = function(newBook) {
-  return this.books.some((book) => book.title === newBook.title)
-}
+  return this.books.some((book) => book.title === newBook.title);
+};
 
 const library = new Library();
-const book1 = new Book("AAA", "aaa", 100, true);
-library.addBook(book1);
-console.log(library);
+
+// User Interface
+const addBookButton = document.getElementById('add-book-btn');
+const addBookModal = document.getElementById('add-book-modal');
+const errorMsg = document.getElementById('error-msg');
+const overlay = document.getElementById('overlay');
+const addBookForm = document.getElementById('add-book-form');
+const booksGrid = document.getElementById('books-grid');
+
+const openAddBookModal = () => {
+  addBookForm.reset();
+  addBookModal.classList.add('active');
+  overlay.classList.add('active');
+};
+
+const closeAddBookModal = () => {
+  addBookModal.classList.remove('active');
+  overlay.classList.remove('active');
+  errorMsg.classList.remove('active');
+  errorMsg.textContent = '';
+};
+
+addBookButton.onclick = openAddBookModal;
+overlay.onclick = closeAddBookModal;
